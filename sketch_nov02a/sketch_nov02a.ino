9bb0fbc00e4b107c310c012a3804c07ef104c2dd
Khalid <58151955+HikaruSadashi@users.noreply.github.com>
2021-11-03 20:04:45 -0400

    Got Resources down, scope down, first commit

---
+++ sketch_nov02a/sketch_nov02a.ino
@@ -0,0 +1,18 @@
+
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+  // pre check for some values
+
+  //SIMULATION from here now on
+  //variables for the simulation?
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = hallRead();
+  Serial.print("sensor = ");
+  Serial.println(val)
+}
